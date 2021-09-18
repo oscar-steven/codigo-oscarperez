f1036bfe014f478d783918a30250bf55c4b692a0
oscar-steven <88859720+oscar-steven@users.noreply.github.com>
2021-09-18 17:43:58 -0500

    update

---
+++ SEMANA4/DIA3/01-javascript.js
@@ -0,0 +1,9 @@
+// console.log permite mostrar en la consolo de JS, lo que se nos antoje, es muy util para saber que esta pasando internamente dentro de JS
+console.log("Hola Mundo!");
+
+// Variables
+// Palabras reservadas, JS las usa
+// Los nombres de variables son sensibles a Mayus y Minus
+var nombre = "Oscar";
+
+console.log(nombre);
