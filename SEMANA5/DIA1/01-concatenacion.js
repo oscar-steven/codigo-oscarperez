01aaf6f06e8bb90779997ec7790a6cab6f9c5428
oscar-steven <88859720+oscar-steven@users.noreply.github.com>
2021-09-23 18:39:12 -0500

    update

---
+++ SEMANA5/DIA1/01-concatenacion.js
@@ -0,0 +1,12 @@
+let nombre = "Oscar"
+let apellido = "Perez"
+
+// console.log(nombre + " " + apellido)
+
+//TEMPLATE STRING / TEMPLATE LITERAL
+//Backsticks `` ALT + 96
+
+let texto = `Hola soy ${nombre} ${apellido} y soy de Lima`
+console.log(texto)
+
+//PROMPT, capturar datos, del usuario
