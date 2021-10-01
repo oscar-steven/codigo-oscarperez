2fedcf030a06d30d61d56293adddfcb502bb2220
oscar-steven <88859720+oscar-steven@users.noreply.github.com>
2021-09-30 20:26:40 -0500

    update

---
+++ SEMANA6/DIA1/02-metodosarrays.js
@@ -0,0 +1,48 @@
+let frutas = ["fresa", "mango", "piña", "lucuma"];
+
+// for(let i=0; i<frutas.length;i++){
+//     console.log(frutas[i])
+// }
+
+//recuerden los arreglos tambien son objetos
+//forEach(function(){})
+frutas.forEach(function (fru) {
+  console.log(fru);
+});
+
+//foreach por cada elemento que encuentre vas a ejecutar una funcion y cada vez que se ejecute esa funcion vas a recibir c/iem en orden y este item lo podemos utilizar dentro de la funcion
+
+let energia = [89, 90, 86, 85, 100];
+
+//energia se va a dividir en 2
+//como podria tener un nuevo arreglo con los valores que representen la mitad de la energia
+
+//crea un nuevo array pero con informacion transformada
+let energiacima = energia.map(function (energy) {
+  return energy / 2;
+});
+
+console.log(energiacima);
+
+//Filter
+let conductores = [
+  {
+    nombre: "Jose",
+    vacunado: true,
+  },
+  {
+    nombre: "Glenda",
+    vacunado: true,
+  },
+  {
+    nombre: "Osmar",
+    vacunado: false,
+  },
+];
+
+
+let puedePasar = conductores.filter(function(driver){
+    return driver.vacunado == true;
+})
+
+console.log(puedePasar)
