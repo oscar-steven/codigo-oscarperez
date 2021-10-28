1c4a388417d6829d7cfd00151b503d8bdd043d4a
oscar-steven <88859720+oscar-steven@users.noreply.github.com>
2021-10-28 18:59:13 -0500

    update

---
+++ SEMANA9/DIA1/01-proyectovanilla/js/App.js
@@ -0,0 +1,6 @@
+// App.js es mi JS principal
+// 1. Tener MockAPI listo 
+// 2. Obtener los datos (GET) fetch
+// 3. Converti esos datos a HTML entendible por el navegador
+
+
---
+++ SEMANA9/DIA1/01-proyectovanilla/js/Comunicacion.js
@@ -0,0 +1,18 @@
+// Se va encargar de hacer peticiones
+// Constante se pone en mayuscula
+
+const URL = "https://617b321fd842cf001711bdcf.mockapi.io/api/codigo/productos";
+
+const obtenerProductos = async () => {
+  try {
+    //intenta esto
+    //await, le decimos aguanta esto se tiene que resolver
+    // respues del fetch, me da el status, 200, cabeceras del response, NO ME DA UN JSON
+    const respuesta = await fetch(URL); //solamente la URL, es un GET, respuesta de la peticion
+    const productos = await respuesta.json(); //aqui hay otra promesa y esto me da el JSON convertido
+    return productos; //resolve(productos)
+  } catch (error) {
+    // y si falla me pasas la voz
+    throw error;
+  }
+};
